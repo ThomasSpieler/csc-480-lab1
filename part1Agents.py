6d2a4fe26ad3b521c704eb2c9dd8c5d1fdd7d656
ThomasSpieler <155924976+ThomasSpieler@users.noreply.github.com>
2026-04-22 22:13:51 -0700

    Final Draft

--- part1Agents.py
+++ part1Agents.py
@@ -252,10 +252,10 @@ class WizardAstar(WizardSearchAgent):
             source_path = self.paths[source_SearchState]
             target_path = [action] + source_path[1]
             cost = len(target_path)
-            self.paths[target_SearchState] = (cost + self.heuristic(target), target_path)
+            self.paths[target_SearchState] = (cost, target_path)
         else:
             cost = 1
-            self.paths[target_SearchState] = (cost + self.heuristic(target), [action])
+            self.paths[target_SearchState] = (cost, [action])
             # Job done
         # Job done.
 
@@ -272,20 +272,124 @@ class WizardAstar(WizardSearchAgent):
 
 
 class CrystalSearchWizard(WizardSearchAgent):
-    # TODO: YOUR CODE HERE
+    @dataclass(eq=True, frozen=True, order=True)
+    class SearchState:
+        wizard_loc: Location
+        portal_loc: Location
+        crystal_count: int
+
+    paths: dict[SearchState, tuple[float, list[WizardMoves]]] = {}
+    search_pq: list[tuple[float, SearchState]] = []
+    initial_game_state: GameState
+
+    def search_to_game(self, search_state: SearchState) -> GameState:
+        initial_wizard_loc = self.initial_game_state.active_entity_location
+        initial_wizard = self.initial_game_state.get_active_entity()
+
+        new_game_state = (
+            self.initial_game_state.replace_entity(
+                initial_wizard_loc.row, initial_wizard_loc.col, EmptyEntity()
+            )
+            .replace_entity(
+                search_state.wizard_loc.row, search_state.wizard_loc.col, initial_wizard
+            )
+            .replace_active_entity_location(search_state.wizard_loc)
+        )
+
+        return new_game_state
+
+    def game_to_search(self, game_state: GameState) -> SearchState:
+        wizard_loc = game_state.active_entity_location
+        portal_loc = game_state.get_all_tile_locations(Portal)[0]
+        crystal_count = len(game_state.get_all_tile_locations(Crystal))
+        return self.SearchState(wizard_loc, portal_loc, crystal_count)
 
     def __init__(self, initial_state: GameState):
         self.start_search(initial_state)
 
+    def start_search(self, game_state: GameState):
+        self.initial_game_state = game_state
+
+        initial_search_state = self.game_to_search(game_state)
+        self.paths = {}
+        self.paths[initial_search_state] = 0, []
+        self.search_pq = [(0, initial_search_state)]
+
+    def is_goal(self, state: SearchState) -> bool:
+        return state.wizard_loc == state.portal_loc
+
+    def cost(self, source: GameState, target: GameState, action: WizardMoves) -> float:
+        return 1
+
+    def __init__(self, initial_state: GameState):
+        self.start_search(initial_state)
+
+    def manhattan_dist(self, loc1: Location, loc2: Location) -> float:
+        # Manhattan Distance
+        return abs(loc1.row - loc2.row) + abs(loc1.col - loc2.col)
+        # Job done
+
+    def heuristic(self, h_target: GameState) -> float:
+        # If no crystals left, just calculate dist to the portal
+        h_target_SearchState = self.game_to_search(h_target)
+        if not h_target.get_all_tile_locations(Crystal):
+            return self.manhattan_dist(h_target_SearchState.wizard_loc, h_target_SearchState.portal_loc)
+        # Job done.
+
+        # Distance to nearest crystal.
+        crystal_dist = min(self.manhattan_dist(h_target_SearchState.wizard_loc, loc) for loc in crystal_dist)
+        # Job done.
+
+        # Longest distance between a crystal and another crystal or the portal
+        targets = h_target.get_all_entity_locations(Crystal) + [h_target_SearchState.portal_loc]
+        max_dist = 0
+
+        for i in range(len(targets)):
+            for j in range(i + 1, len(targets)):
+                dist = self.manhattan_dist(targets[i], targets[j])
+                if dist > max_dist:
+                    max_dist = dist
+        # Job done.
+
+        # Sum the distances and return
+        return crystal_dist + max_dist
+        # Job done.
+
+
     def next_search_expansion(self) -> GameState | None:
-        # TODO YOUR CODE HEREs
-        raise NotImplementedError
+        # Chooses the next search node to expand
+        return_node = heapq.heappop(self.search_pq)[1]
+        # Add the node we're expanding to the list of searched nodes
+        return self.search_to_game(return_node)
 
     def process_search_expansion(
         self, source: GameState, target: GameState, action: WizardMoves
     ) -> None:
-        # TODO YOUR CODE HERE
-        raise NotImplementedError
+        # Update paths.
+        target_SearchState = self.game_to_search(target)
+        target_heuristic = self.heuristic(target)
+        source_SearchState = self.game_to_search(source)
+            # If we know the path to the start, preface the path to the target with the source's path. Otherwise (like at the start), start a new path.
+        if (source_SearchState in self.paths):
+            source_path = self.paths[source_SearchState]
+            target_path = [action] + source_path[1]
+            cost = len(target_path)
+            self.paths[target_SearchState] = (cost, target_path)
+        else:
+            cost = 1
+            self.paths[target_SearchState] = (cost + target_heuristic, [action])
+            # Job done
+        # Job done.
+
+        insert_candidate = (cost + target_heuristic, target_SearchState)
+        # Update the list of nodes to expand if it's better than the existing path cost.
+        heapq.heappush(self.search_pq, insert_candidate)
+        # Job done.
+
+        # If the target is the goal, update the class plan to match the path to the target.
+        if (self.is_goal(target_SearchState)):
+            self.plan = self.paths[target_SearchState][1]
+        # Job done.
 
 
 
